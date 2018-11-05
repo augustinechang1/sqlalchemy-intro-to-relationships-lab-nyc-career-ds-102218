1735a34a391f459b0212ccaa0c05f01f446869e5
Augustine Chang <augcha95@gmail.com>
2018-11-05 15:35:24 -0500

    lab done

--- models.py
+++ models.py
@@ -4,14 +4,19 @@ from sqlalchemy.orm import sessionmaker, relationship
 
 Base = declarative_base()
 
+class Actor(Base):
+    __tablename__ = "actors"
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    roles = relationship('Role', back_populates = 'actor')
+
+class Role(Base):
+    __tablename__ = "roles"
+    id = Column(Integer, primary_key = True)
+    character = Column(String)
+    actor_id = Column(Integer, ForeignKey('actors.id'))
+    actor = relationship('Actor', back_populates = 'roles')
 
 # write the Role and Actor classes below
-
-
-
-
-
-
-
-engine = create_engine('sqlite:///actors.db')
+engine = create_engine('sqlite:///actors.db', echo = True)
 Base.metadata.create_all(engine)
